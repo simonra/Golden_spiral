037b869dd20b417cacdd8cbd16562a1d7c8640a9
Simon Randby <simon.randby@gmail.com>
2015-07-21 14:12:08 +0200

    Made new version that is simpler to me
    This thing is somewhat more understandable to me =P
    Still needs some cleanup to see which variables are used, if some
    stuff could be compressed to fewer lines, etc. Maybe removing old
    commented code now that a sketch to what probably is the final solution
    is in place...

--- golden_squares.py
+++ golden_squares.py
@@ -43,39 +43,83 @@ current_longest_horisontal_edge = spiral_size_short_edge
 offset_left = Decimal(0)
 offset_top = Decimal(0)
 current_shortest_horisontal_edge = current_longest_horisontal_edge / golden_ratio
-makeNewSquare(current_longest_horisontal_edge, offset_left, offset_top, colors[iterator])
+# makeNewSquare(current_longest_horisontal_edge, offset_left, offset_top, colors[iterator])
 
-#while(current_shortest_edge > shortest_premissible_edge):
-for x in xrange(1,8):
-	iterator = iterator + 1
-	current_longest_vertical_edge = current_shortest_horisontal_edge
-	current_total_vertical_edge = current_longest_horisontal_edge
-	current_shortest_vertical_edge = current_total_vertical_edge - current_longest_vertical_edge
-	offset_left, offset_top = (offset_left + current_longest_horisontal_edge, offset_top)
-	makeNewSquare(current_shortest_horisontal_edge, offset_left, offset_top, colors[iterator % len(colors)])
-	iterator = iterator + 1
+leftmost_edge = Decimal(0)
+rightmost_edge = spiral_size_long_edge
+top_edge = Decimal(0)
+bottom_edge = spiral_size_short_edge
+edge_size = spiral_size_short_edge
 
-	current_total_horisontal_edge = current_longest_vertical_edge
-	current_longest_horisontal_edge = current_shortest_vertical_edge
-	current_shortest_horisontal_edge = current_total_horisontal_edge - current_longest_horisontal_edge
-	offset_left, offset_top = (offset_left + current_shortest_horisontal_edge, offset_top +current_longest_vertical_edge)
-	makeNewSquare(current_longest_horisontal_edge, offset_left, offset_top, colors[iterator % len(colors)])
-	iterator = iterator + 1
 
-	current_longest_vertical_edge = current_shortest_horisontal_edge
-	current_total_vertical_edge = current_longest_horisontal_edge
-	current_shortest_vertical_edge = current_total_vertical_edge - current_longest_vertical_edge
-	offset_left, offset_top = (offset_left - current_shortest_horisontal_edge, offset_top + current_shortest_vertical_edge)
-	makeNewSquare(current_longest_vertical_edge, offset_left, offset_top, colors[iterator % len(colors)])
+# Part of my solution
+while(edge_size > shortest_premissible_edge):
+# Part of Ilses solution
+# for x in xrange(1,8):
+
+	# Make the leftmost square:
+	edge_size = spiral_size_short_edge / (golden_ratio ** Decimal(iterator))
+	makeNewSquare(edge_size, leftmost_edge, top_edge, colors[iterator % len(colors)])
+	# Only the offset from the left has changed when making this square
+	# therefore only the leftmost edge parameter is updated here
+	leftmost_edge = leftmost_edge + edge_size
 	iterator = iterator + 1
 
-	current_total_horisontal_edge = current_longest_vertical_edge
-	current_longest_horisontal_edge = current_shortest_vertical_edge
-	current_shortest_horisontal_edge = current_total_horisontal_edge - current_longest_horisontal_edge
-	offset_left, offset_top = (offset_left, offset_top - current_shortest_vertical_edge)
-	makeNewSquare(current_longest_horisontal_edge, offset_left, offset_top, colors[iterator % len(colors)])
+	# Make the top right square:
+	edge_size = spiral_size_short_edge / (golden_ratio ** Decimal(iterator))
+	makeNewSquare(edge_size, leftmost_edge, top_edge, colors[iterator % len(colors)])
+	# Update only the topmost edge at this point
+	top_edge = top_edge + edge_size
+	iterator = iterator + 1
 
-	current_shortest_edge = current_shortest_horisontal_edge
+	# Make the bottom right square:
+	edge_size = spiral_size_short_edge / (golden_ratio ** Decimal(iterator))
+	makeNewSquare(edge_size, rightmost_edge - edge_size, top_edge, colors[iterator % len(colors)])
+	# Update the rightmost_edge edge at this point
+	rightmost_edge = rightmost_edge - edge_size
+	iterator = iterator + 1
+
+	# Make the bottom left (central) square of this iteration:
+	edge_size = spiral_size_short_edge / (golden_ratio ** Decimal(iterator))
+	makeNewSquare(edge_size, leftmost_edge, bottom_edge - edge_size, colors[iterator % len(colors)])
+	# update bottom_edge:
+	bottom_edge = bottom_edge - edge_size
+	iterator = iterator + 1
+
+
+	# Ilses version
+	# iterator = iterator + 1
+	# current_longest_vertical_edge = current_shortest_horisontal_edge
+	# current_total_vertical_edge = current_longest_horisontal_edge
+	# current_shortest_vertical_edge = current_total_vertical_edge - current_longest_vertical_edge
+	# offset_left, offset_top = (offset_left + current_longest_horisontal_edge, offset_top)
+	# makeNewSquare(current_shortest_horisontal_edge, offset_left, offset_top, colors[iterator % len(colors)])
+	# iterator = iterator + 1
+
+	# current_total_horisontal_edge = current_longest_vertical_edge
+	# current_longest_horisontal_edge = current_shortest_vertical_edge
+	# current_shortest_horisontal_edge = current_total_horisontal_edge - current_longest_horisontal_edge
+	# offset_left, offset_top = (offset_left + current_shortest_horisontal_edge, offset_top +current_longest_vertical_edge)
+	# makeNewSquare(current_longest_horisontal_edge, offset_left, offset_top, colors[iterator % len(colors)])
+	# iterator = iterator + 1
+
+	# current_longest_vertical_edge = current_shortest_horisontal_edge
+	# current_total_vertical_edge = current_longest_horisontal_edge
+	# current_shortest_vertical_edge = current_total_vertical_edge - current_longest_vertical_edge
+	# offset_left, offset_top = (offset_left - current_shortest_horisontal_edge, offset_top + current_shortest_vertical_edge)
+	# makeNewSquare(current_longest_vertical_edge, offset_left, offset_top, colors[iterator % len(colors)])
+	# iterator = iterator + 1
+
+	# current_total_horisontal_edge = current_longest_vertical_edge
+	# current_longest_horisontal_edge = current_shortest_vertical_edge
+	# current_shortest_horisontal_edge = current_total_horisontal_edge - current_longest_horisontal_edge
+	# offset_left, offset_top = (offset_left, offset_top - current_shortest_vertical_edge)
+	# makeNewSquare(current_longest_horisontal_edge, offset_left, offset_top, colors[iterator % len(colors)])
+
+	# current_shortest_edge = current_shortest_horisontal_edge
+
+
+	# Old ramblings:
 	# makeNewSquare(current_shortest_edge, offset_left, offset_top, colors[iterator % len(colors)])
 	# iterator = iterator + 1
 	# current_shortest_edge = current_shortest_edge / (golden_ratio ** Decimal(iterator))
